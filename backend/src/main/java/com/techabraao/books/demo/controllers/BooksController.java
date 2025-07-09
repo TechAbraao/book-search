f8096a3f76be696e997e1152cf40cc65c9faeba2
Abraão Santos <abraao.santos@sec4you.com.br>
2025-07-09 12:26:00 -0300

    feat: adding docker docs and starting postgreSQL database and API

---
+++ backend/src/main/java/com/techabraao/books/demo/controllers/BooksController.java
@@ -0,0 +1,12 @@
+package com.techabraao.books.demo.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+@RequiredArgsConstructor
+public class BooksController {
+
+}
